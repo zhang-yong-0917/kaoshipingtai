54fc654306cb1b9d2c0eb0355403a3e6d310b77b
zhang-yong-0917 <1454890198@qq.com>
2021-10-19 22:36:22 +0800

    随机抽取题目——1

--- src/main/java/kaoshipingtai/entity/Teacher.java
+++ src/main/java/kaoshipingtai/entity/Teacher.java
@@ -1,5 +1,31 @@
 package kaoshipingtai.entity;
 
 public class Teacher {
+private  int TeacherId;
+private  String TeacherName;
+private String TeacherPassword;
 
+    public int getTeacherId() {
+        return TeacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.TeacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return TeacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.TeacherName = teacherName;
+    }
+
+    public String getTeacherPassword() {
+        return TeacherPassword;
+    }
+
+    public void setTeacherPassword(String teacherPassword) {
+        this.TeacherPassword = teacherPassword;
+    }
 }
